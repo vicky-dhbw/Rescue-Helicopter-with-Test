8676f8fdf630d5f6f75da8726bd2d9ec98c776b8
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-05-26 10:14:50 +0200

    comments added to my droneComponents!

--- src/main/java/DroneComponents/DroneCentralUnit.java
+++ src/main/java/DroneComponents/DroneCentralUnit.java
@@ -8,6 +8,8 @@ public class DroneCentralUnit {
 
     Camera camera;
     Storage storage;
+
+    //since drone central unit must control the camera and storage, the two are given as parameter in default constructor
     public DroneCentralUnit(Camera camera, Storage storage) {
         this.camera=camera;
         this.storage=storage;
@@ -16,8 +18,10 @@ public class DroneCentralUnit {
     public void switchOnCamera(){
         camera.setIsOn(true);
         System.out.println("Camera switched on..");
-        storage.loadContent();
-       searchHuman();
+        storage.loadContent();                                //when the camera is switched on the contents are loaded...
+                                                             // Note loadContent has both setContent and placeHuman methods
+        searchHuman();                                      //after loading contents off all sections of the landscape...
+                                                           // the drone searches for "human" placed in a random section in random coordinates
     }
     public void switchOffCamera(){
         camera.setIsOn(false);
@@ -40,7 +44,7 @@ public class DroneCentralUnit {
 
         for (int i = 0; i < 50; i++) {
             for (int j = 0; j < 50; j++) {
-                section = storage.getSections()[i][j];
+                section = storage.getSections()[i][j];                //first getting section from storage and then looking in it for "human" through 2 loops
                 for (int k = 0; k < 50; k++) {
                     for (int l = 0; l < 50; l++) {
                         arrayCopy[l] = section.getContent()[k][l];
