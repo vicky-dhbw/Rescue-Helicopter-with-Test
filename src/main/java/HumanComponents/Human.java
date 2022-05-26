7c6ee5e3ed3c610ff0f15829feeac66b32ca5594
1-B768 <bernhard.schwab98@web.de>
2022-05-26 18:06:19 +0200

    Human correction with class Patient extends Human

--- src/main/java/HumanComponents/Human.java
+++ src/main/java/HumanComponents/Human.java
@@ -1,32 +1,36 @@
 package HumanComponents;
 
 public class Human {
-    private final String firstName;
-    private final String secondName;
-    private final Gender gender;
+    private String firstName;
+    private String secondName;
+    private Gender gender;
     private String birthDate;
     private final boolean smoker;
-    private final boolean vaccinated;
+    private boolean vaccinated;
     private final Lung leftLung;
     private final Lung rightLung;
     private boolean isAlive;
     private boolean lungsAreFilled;
+    private boolean isInfected;
 
-    public Human(boolean isInvected, String firstName, String secondName, Gender gender, String birthDate, boolean smoker, boolean vaccinated) {
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.gender = gender;
+    public Human(boolean isInfected, boolean smoker) {
         this.birthDate = birthDate;
         this.smoker = smoker;
-        this.vaccinated = vaccinated;
-        leftLung = new Lung(smoker, isInvected);
-        rightLung = new Lung(smoker, isInvected);
+        this.isInfected = isInfected;
+        leftLung = new Lung(smoker, isInfected);
+        rightLung = new Lung(smoker, isInfected);
         leftLung.setCells();
         rightLung.setCells();
         isAlive = true;
         lungsAreFilled = false;
     }
-    private boolean choke(){
+    public void setHumanProperties(String firstName, String secondName, Gender gender, boolean vaccinated){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.gender = gender;
+        this.vaccinated = vaccinated;
+    }
+    public boolean choke(){
         boolean choke = false;
         double damagedCellsLeftLung = leftLung.getNumberOfDamagedCells()+ leftLung.getNumberOfInfectedCells();
         double damagedCellsRightLung = rightLung.getNumberOfDamagedCells()+ rightLung.getNumberOfInfectedCells();
---
+++ src/main/java/HumanComponents/Patient.java
@@ -0,0 +1,7 @@
+package HumanComponents;
+
+public class Patient extends Human{
+    public Patient(boolean smoker){
+        super(true, smoker);
+    }
+}
